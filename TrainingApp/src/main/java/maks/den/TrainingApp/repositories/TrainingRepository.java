b2c9fdf9a5939632b3f5f3e26e50885591e1656c
Maksymilian Dendura <maksymilian.dendura@gmail.com>
2024-06-03 19:36:22 +0200

    Created Training Repository

---
+++ TrainingApp/src/main/java/maks/den/TrainingApp/repositories/TrainingRepository.java
@@ -0,0 +1,12 @@
+package maks.den.TrainingApp.repositories;
+
+import maks.den.TrainingApp.entity.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TrainingRepository extends JpaRepository<Training, Integer> {
+    @Query(value = "SELECT * FROM TRAINING", nativeQuery = true)
+    List<Training> findAll();
+}
