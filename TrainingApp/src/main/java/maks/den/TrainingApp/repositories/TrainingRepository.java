b6d2d3f85acb32bb3cd26a82241bf63672869766
Maksymilian Dendura <maksymilian.dendura@gmail.com>
2024-06-03 21:27:41 +0200

    Added average training duration endpoint

--- TrainingApp/src/main/java/maks/den/TrainingApp/repositories/TrainingRepository.java
+++ TrainingApp/src/main/java/maks/den/TrainingApp/repositories/TrainingRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface TrainingRepository extends JpaRepository<Training, Integer> {
     @Query(value = "SELECT * FROM TRAINING", nativeQuery = true)
     List<Training> findAll();
+
+    @Query(value = "SELECT AVG(duration) FROM TRAINING", nativeQuery = true)
+    Number averageDuration();
 }
