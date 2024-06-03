b7257a68eacbad95631d34567275a05838713c49
Maksymilian Dendura <maksymilian.dendura@gmail.com>
2024-06-03 18:39:28 +0200

    Initialize Spring

---
+++ TrainingApp/src/main/java/maks/den/TrainingApp/TrainingAppApplication.java
@@ -0,0 +1,13 @@
+package maks.den.TrainingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAppApplication.class, args);
+	}
+
+}
