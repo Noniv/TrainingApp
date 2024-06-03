4031abe9a91b39fbd9be7f428518f884f3d118bb
Maksymilian Dendura <maksymilian.dendura@gmail.com>
2024-06-03 21:28:29 +0200

    Added main form

--- TrainingAppUI/src/app/app.component.ts
+++ TrainingAppUI/src/app/app.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+import { HttpService } from './services/http.service';
+import { Training } from './models/training';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'TrainingAppUI';
+export class AppComponent implements OnInit {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  trainingFormGroup!: FormGroup;
+  trainings: Training[] = [];
+  isEditMode = false;
+  averageDuration = 0;
+
+  constructor(
+    private _snackBar: MatSnackBar,
+    private formBuilder: FormBuilder,
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit(): void {
+    this.trainingFormGroup = this.formBuilder.group({
+      id: [''],
+      date: ['', Validators.required],
+      description: ['', Validators.required],
+      duration: ['', Validators.required],
+      calories: ['', Validators.required],
+    });
+    this.getTraining();
+    this.getAverageDuration();
+  }
+
+  openSnackBar(message: string) {
+    this._snackBar.open(message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: 2000,
+    });
+  }
+
+  getTraining() {
+    this.httpService.getTraining().subscribe((data) => {
+      this.trainings = data;
+    });
+  }
+
+  getAverageDuration() {
+    this.httpService.getAverageDuration().subscribe((data) => {
+      this.averageDuration = data;
+    });
+  }
+
+  deleteTraining(id: number | undefined) {
+    if (id) {
+      this.httpService.deleteTraining(id).subscribe((data) => {
+        this.getTraining();
+        this.getAverageDuration();
+        this.openSnackBar("Usunięto trening")
+      });
+    }
+  }
+
+  handleUpdateTraining(training: Training) {
+    this.isEditMode = true;
+    this.trainingFormGroup.setValue(training);
+  }
+
+  updateTraining() {
+    const formValue: Training = this.trainingFormGroup.value;
+
+    this.httpService.updateTraining(formValue).subscribe((data) => {
+      this.trainingFormGroup.reset();
+      this.isEditMode = false;
+      this.openSnackBar("Zaktualizowano trening")
+      this.getTraining();
+      this.getAverageDuration();
+    });
+  }
+
+  onSubmit() {
+    if (this.trainingFormGroup.invalid) {
+      return;
+    }
+    if (this.isEditMode) {
+      this.updateTraining();
+    } else {
+      const formValue: Training = this.trainingFormGroup.value;
+      const trainingRequest: Training = {
+        date: formValue.date,
+        description: formValue?.description,
+        duration: formValue?.duration,
+        calories: formValue?.calories,
+      };
+      this.httpService.createTraining(trainingRequest).subscribe((data) => {
+        this.getTraining();
+        this.getAverageDuration();
+        this.openSnackBar("Dodano trening")
+      });
+    }
+  }
 }
--- TrainingAppUI/src/app/app.module.ts
+++ TrainingAppUI/src/app/app.module.ts
@@ -4,17 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
